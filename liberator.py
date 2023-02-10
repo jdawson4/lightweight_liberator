ac0cee027a27d0da0858bc7d46980f181ed968e0
Jacob Dawson <jacob.dawson@me.com>
2023-02-10 17:16:47 -0500

    works for more FITS types

--- liberator.py
+++ liberator.py
@@ -6,15 +6,24 @@
 
 from astropy.io import fits
 import numpy as np
-from os import listdir
-from os.path import isfile, join
+import os
 import imageio
 
-onlyfiles = [f for f in listdir('FITS') if isfile(join('FITS', f))]
-imgCount=0
-for file in onlyfiles:
-    with fits.open('FITS/' + file) as hdul:
-        data = hdul[1].data
+def processFile(path,filename):
+    with fits.open(path) as hdul:
+        dataFound = False
+        for i in range(2):
+            h = hdul[i]
+            if h.data is None:
+                continue
+            if len(h.data.shape) == 2:
+                dataFound = True
+                data = h.data
+                break
+        if not dataFound:
+            return
+        data = data.astype(np.float32)
+        data = np.nan_to_num(data)
         data = np.clip(data, np.percentile(data, 20.0), np.percentile(data, 99.625))
         if (np.amin(data) < 0.0):
             data += np.amin(data)
@@ -22,5 +31,14 @@ for file in onlyfiles:
             data -= np.amin(data)
         data /= np.amax(data)
         data *= 255.0
-        imgCount+=1
-        imageio.imwrite('pngs/'+str(imgCount)+'.png', data)
+        print('writing',filename[:-5]+'.png')
+        imageio.imwrite('pngs/'+filename[:-5]+'.png', data.astype(np.uint8))
+
+list_of_files = {}
+for (dirpath, dirnames, filenames) in os.walk('FITS'):
+    for filename in filenames:
+        if filename.endswith('.fits'):
+            list_of_files[filename] = os.sep.join([dirpath, filename])
+
+for k,v in list_of_files.items():
+    processFile(v, k)
